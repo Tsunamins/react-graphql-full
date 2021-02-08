140aa19747440290a8fddb359d1da1564876f298
Alison Reilly <reillyamr@gmail.com>
2021-02-08 15:52:17 -0500

    add additional query and mutation

--- hackernews-react-apollo/src/components/CreateLink.js
+++ hackernews-react-apollo/src/components/CreateLink.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
-
-import { useMutation, gql } from '@apollo/client';
+import { gql, useMutation } from '@apollo/client';
+import { useHistory } from 'react-router';
+import { LINKS_PER_PAGE } from '../constants';
+import { FEED_QUERY } from './LinkList';
 
 const CREATE_LINK_MUTATION = gql`
   mutation PostMutation(
@@ -9,25 +11,59 @@ const CREATE_LINK_MUTATION = gql`
   ) {
     post(description: $description, url: $url) {
       id
-      createdAt
       url
       description
     }
   }
 `;
 
-
 const CreateLink = () => {
+  const history = useHistory();
   const [formState, setFormState] = useState({
     description: '',
     url: ''
   });
+  const [createLink] = useMutation(CREATE_LINK_MUTATION, {
+    variables: {
+      description: formState.description,
+      url: formState.url
+    },
+    update: (cache, { data: { post } }) => {
+      const take = LINKS_PER_PAGE;
+      const skip = 0;
+      const orderBy = { createdAt: 'desc' };
 
+      const data = cache.readQuery({
+        query: FEED_QUERY,
+        variables: {
+          take,
+          skip,
+          orderBy
+        }
+      });
+
+      cache.writeQuery({
+        query: FEED_QUERY,
+        data: {
+          feed: {
+            links: [post, ...data.feed.links]
+          }
+        },
+        variables: {
+          take,
+          skip,
+          orderBy
+        }
+      });
+    },
+    onCompleted: () => history.push('/new/1')
+  });
   return (
     <div>
       <form
         onSubmit={(e) => {
           e.preventDefault();
+          createLink();
         }}
       >
         <div className="flex flex-column mt3">
